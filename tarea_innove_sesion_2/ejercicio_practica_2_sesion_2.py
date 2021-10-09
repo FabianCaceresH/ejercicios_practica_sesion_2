f3d118ba913fc9f51f2d08cc17af475c6f2337f0
FabianCaceresH <90655920+FabianCaceresH@users.noreply.github.com>
2021-10-09 15:45:14 -0500

    Add files via upload

---
+++ tarea_innove_sesion_2/ejercicio_practica_2_sesion_2.py
@@ -0,0 +1,30 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica
+
+numero_1 = 5
+numero_2 = 7
+
+# Realizar la suma de las dos variables
+print(f"la suma de numero1 y numero2 es {numero_1+numero_2}")
+# numero_1 y numero_2
+# Almacenar el valor de la suma en una variable
+a = numero_1+numero_2
+# ej:
+# operacion = .....
+
+# Imprimir en pantalla el resultado de la suma
+# print(....)
+print(f"la suma de los numeros pero hecha variable{a}")
+
+# Repita el procedimiento para realizar la resta
+print(f"la resta de numero 1 y numero2 es {numero_1-numero_2}")
+b = numero_1-numero_2
+print(f"la resta de los numeros pero hecha variable es {b}")
---
+++ tarea_innove_sesion_2/ejercicio_practica_3_sesion_2.py
@@ -0,0 +1,29 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese primero su nombre y luego su apellido
+# Capture ambos datos e imprima su nombre completo
+print('Ingrese por consola su nombre/s:')
+nombre = str(input())
+
+print('Ingrese por consola su apellido/s:')
+apellido = str(input())
+nombre_completo = nombre +" " +apellido
+print(nombre_completo)
+# Imprima su nombre d
+
+# Almacenar su nombre completo en una variable
+# nombre_completo = .....
+
+# Imprimir la cantidad de letras que posee su nombre completo
+# cantidad_letras = len(....)
+papa = len(nombre)
+print(f"la cantidad de caracteres que posee el nombre completo es {papa}")
